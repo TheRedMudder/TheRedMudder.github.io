e13d5831211a5898ab7acbdec10777c2c1dfe572
TheRedMudder <ron.dias@outlook.com>
2017-06-02 17:47:55 -0400

    Fixed Error of face.jpg not loading and ?ver=4.7.5 not being found

--- me/fixhost.py
+++ me/fixhost.py
@@ -30,6 +30,10 @@ def replaceAllfolder(filetype,old_text,new_text):
 
 # Fix Host
 replaceAllfolder(".html",'localhost','theredmudder.github.io');
+#Fix Dynamic Problem
+replaceAllfolder(".html",'?ver=4.7.5','');
+#Fixes Broken Image
+replaceAllfolder(".html",'http://theredmudder.github.io/me/wp-content/uploads/2016/09/face.jpg','https://demo.themegrill.com/flash-one-page/wp-content/uploads/sites/93/2016/09/face.jpg');
 #Removes Extra Index Files
 checkforgarbage = glob.glob("*.html");
 for checkgarb in checkforgarbage:
